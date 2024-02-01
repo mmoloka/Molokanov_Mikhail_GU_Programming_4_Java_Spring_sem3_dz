ed2d87cabd8662f7ad91e9ab278fa4c6a332cdd7
mmoloka <kopfgolova@gmail.com>
2024-02-01 11:27:32 +0300

    Sem7_1dz Commit

--- sem3_dz/src/main/java/org/example/sem3_dz/security/CustomUserDetailsService.java
+++ sem3_dz/src/main/java/org/example/sem3_dz/security/CustomUserDetailsService.java
@@ -14,11 +14,11 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private  final UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByLogin(username).orElseThrow(()-> new UsernameNotFoundException(username));
+        User user = userRepository.findByLogin(username).orElseThrow(() -> new UsernameNotFoundException(username));
         return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), List.of(
                 new SimpleGrantedAuthority("admin"),
                 new SimpleGrantedAuthority("user")
